423c2121b684a9b4375f58f95310a9b1114dc10a
Khuirul-Huda <hudah2102@gmail.com>
2024-11-27 17:43:27 +0700

    init: initialize Flask app with Tailwind CSS setup and basic routing

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.get('/app')
+def launch():
+    return render_template('app.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
